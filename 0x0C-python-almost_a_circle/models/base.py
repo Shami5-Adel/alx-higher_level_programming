6fba278e30e72fa8e23c49313829897b743effb1
Shami5-Adel <dshamiade@gmail.com>
2024-04-24 12:27:39 -0700

    almostacircle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+""" Defines a base model class """
+
 class Base:
 __nb_objects = 0
+
+
 def __init__(self, id=None):
+    """ Initialize a new Base """
     if id is not None
     self.id = id
 else:
     Base__nb_objects += 1
     self.id = Base__nb_objects
-
-
